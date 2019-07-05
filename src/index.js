54552e83251953f7d765c05a755ae6c744e1f245
sry1 <steve.roy99@yahoo.com>
2019-07-05 16:54:50 +0300

    module 9 work in progress

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './index.css';
+import numeral from 'numeral';
+
+/* eslint-disable no-console */
+
+const courseValue=numeral(1000).format('$0,0.00');
+
+/* eslint-disable no-debugger */
+
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
